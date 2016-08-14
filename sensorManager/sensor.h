b79232cfdc156af84c99d9811bb265a0defa324c
Aurelian Zanoschi <aurelian17@gmail.com>
2016-08-14 15:43:28 +0300

    Removed wired specifics from sensor
    Removed all the wired sensor specific values from the generic sensor
    class.
    The interrupt function is now pure virtual in order to force to be
    implemented in derived classes.
    Some optional parameters were removed in order to avoid confusion.

--- sensorManager/sensor.h
+++ sensorManager/sensor.h
@@ -5,64 +5,39 @@
 
 using namespace piHome;
 
-class sensorInstantiator;
-
 class sensor
 {
 public:
     explicit sensor(const SystemType&,
                     const SensorType&,
-                    const HardwareType&,
                     const QString&,
                     const QString&,
-                    const QString&,
-                    const QString &value = QString(""),
-                    const bool &interrupt = false);
-    sensor(const sensor &obj);
-    sensor& operator=(const sensor &other);
+                    const QString&);
+    sensor(const sensor&);
+    sensor& operator=(const sensor&);
     virtual ~sensor();
 
-    friend class sensorInstantiator;
-
-public:
     SystemType getSystemType() const;
     SensorType getSensorType() const;
-    HardwareType getHardwareType() const;
     QString getZone() const;
     QString getNode() const;
     QString getAddress() const;
     QString getValue() const;
-    bool getSupportInterrupt() const;
-
-    QString getWiredEdge() const;
-    int getWiredTimeout() const;
 
     void setValue(const QString &value);
-    void setSupportInterrupt(const bool &value);
 
     void debugSensor() const;
 
-protected:
-    void setWiredEdge(const QString &edge);
-    void setWiredTimeout(const int &timeout);
+public slots:
+    virtual void interrupt(void) = 0;
 
 private:
     SystemType m_system;
     SensorType m_type;
-    HardwareType m_hard;
     QString m_zone;
     QString m_node;
     QString m_address;
     QString m_value;
-    bool m_supportInterrupt;
-
-    QString m_wiredEdge;
-    int m_wiredTimeout;
-
-    template<class T> friend class QList;
-
-public slots:
-    void wiredInterrupt(void);
 };
 
 #endif // SENSOR_H

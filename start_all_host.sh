b495a62e119588301bba66acaea13ec0cd93eb64
Aurelian Zanoschi <aurelian17@gmail.com>
2017-02-05 21:49:48 +0200

    Added helper scripts for host development
    The scripts for starting and stopping all the services. The scripts
    help a log on launching, stopping all services when working on
    host.

---
+++ start_all_host.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+sudo ls -las
+
+if pidof -x "databaseManager" >/dev/null;
+then
+	echo "DatabaseManager already started"
+else
+	sudo /usr/share/pismarthome/databaseManager &
+fi;
+
+if pidof -x "mailManager" >/dev/null;
+then
+	echo "MailManager already started"
+else
+	sudo /usr/share/pismarthome/mailManager &
+fi;
+
+if pidof -x "userManager" >/dev/null;
+then
+	echo "UserManager already started"
+else
+	sudo /usr/share/pismarthome/userManager &
+fi;
+
+if pidof -x "sensorManager" >/dev/null;
+then
+	echo "SensorManager already started"
+else
+	sudo /usr/share/pismarthome/sensorManager &
+fi;
+
+if pidof -x "uiManager" >/dev/null;
+then
+	echo "UIManager already started"
+else
+	sudo /usr/share/pismarthome/uiManager &
+fi;
---
+++ stop_all_host.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo kill -2 `pidof uiManager`
+
+sudo kill -2 `pidof sensorManager`
+
+sudo kill -2 `pidof userManager`
+
+sudo kill -2 `pidof mailManager`
+
+sudo kill -2 `pidof databaseManager`
